8debfab708d41d415eed49f2d043e07eca4117d0
AndreOliveiraMendes <ao_mendes@hotmail.com>
2026-04-17 22:30:58 -0300

    update .gitignore to include logs directory and reorder .env entry; modify main function to log CPU temperature data

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,13 @@
+from app.battery import get_power
+from app.cpu import get_cpu_temps
+from app.log import log_data
+
+
 def main():
-    print("hello world")
+    # temperature
+    data = get_cpu_temps()
+    
+    print(data)
     
 if __name__ == "__main__":
     main()
--- app/log.py
+++ app/log.py
@@ -7,6 +7,8 @@ from config import LOG_FOLDER
 def log_data(data, filename):
     if not LOG_FOLDER or not filename:
         return
+    if not os.path.isdir(LOG_FOLDER):
+        os.makedirs(LOG_FOLDER)
     LOG_FILE = os.path.join(LOG_FOLDER, filename)
     with open(LOG_FILE, "a", encoding='utf-8') as f:
         f.write(json.dumps(data) + "\n")
